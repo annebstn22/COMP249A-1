ac14e096f29252912de792619125b6aead78f30f
Anne <annebstn22@hotmail.ca>
2021-01-25 23:01:40 +0000

    double arrays for setting ladders and snakes

--- LadderAndSnake.java
+++ LadderAndSnake.java
@@ -5,6 +5,10 @@ public class LadderAndSnake{
     private int nbPlayers;
     private final static int NB_DICE_FACES = 6;
     private final static int BOARD_SIZE = 10;
+    private final static int NB_SNAKES = 8;
+    private final static int NB_LADDERS = 9;
+    private final static int SNAKE_HEADS_AND_TAILS[][] = new int[NB_SNAKES][2];
+    private final static int LADDER_TOP_AND_BOTTOM[][] = new int[NB_LADDERS][2];
 
     public LadderAndSnake(int nbPlayers) {
         this.nbPlayers = nbPlayers;
