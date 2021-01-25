913404ee27730a00b6b3e588cc2f0ac53a11c8f4
Anne <annebstn22@hotmail.ca>
2021-01-25 22:45:50 +0000

    Created a player class

---
+++ Player.java
@@ -0,0 +1,13 @@
+public class Player {
+
+    private int currentPosition = 0;
+
+    public int getCurrentPosition(){
+        return this.currentPosition;
+    }
+
+    public void setCurrentPosition(int newPosition){
+        currentPosition = newPosition;
+    }
+    
+}
