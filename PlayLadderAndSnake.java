cdd15bb38129fb86ff315b3fac2d2f81fb9c669f
Anne <annebstn22@hotmail.ca>
2021-01-25 20:49:44 +0000

    made driver

---
+++ PlayLadderAndSnake.java
@@ -0,0 +1,18 @@
+public class PlayLadderAndSnake {
+
+    public static void main(String[] args) {
+        
+        LadderAndSnake hello = new LadderAndSnake(3);
+
+        System.out.println(LadderAndSnake.flipDice());
+        System.out.println(LadderAndSnake.flipDice());
+        System.out.println(LadderAndSnake.flipDice());
+        System.out.println(LadderAndSnake.flipDice());
+        System.out.println(LadderAndSnake.flipDice());
+        System.out.println(LadderAndSnake.flipDice());
+        System.out.println(LadderAndSnake.flipDice());
+        System.out.println(LadderAndSnake.flipDice());
+
+    }
+    
+}
