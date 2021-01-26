86705f860f831d9c4b35dda4b6ec1a480b07a730
Anne <annebstn22@hotmail.ca>
2021-01-26 21:26:58 +0000

    format

--- Playsnakeandladder.java
+++ Playsnakeandladder.java
@@ -28,7 +28,7 @@ public class Playsnakeandladder {
 		for (int i=0; i < numPlayers; i++) {
 			playerArray[i] = new Player("player"+Integer.toString(i+1));
 		}
-			
+		
 		System.out.println("you've chosen " + numPlayers + " players");
 		
 		LadderAndSnake game = new LadderAndSnake(numPlayers, playerArray);
