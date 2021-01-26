5ce5d5b35ac814aea8d5ab8b1368a01585a88853
Anne <annebstn22@hotmail.ca>
2021-01-26 03:51:45 +0000

    added attribute name for player

--- Player.java
+++ Player.java
@@ -1,6 +1,15 @@
 public class Player {
 
+    private String name;
     private int currentPosition = 0;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
 
     public int getCurrentPosition(){
         return this.currentPosition;
