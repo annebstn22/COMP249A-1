0c2ea87aa68d00722f0f1eda3a37fb23aa1286fa
Anne <annebstn22@hotmail.ca>
2021-02-07 02:32:29 +0000

    renamed driver according to assignment instrc.

--- PlayLadderAndSnake.java
+++ PlayLadderAndSnake.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 /**
- * Playsnakeandladder
+ * PlayLadderAndSnake
  * plays the ladder and snake game where 2 to 4 people roll a dice and move on up the board with the added chance of 
  * going up or down depending on whether they land on a ladder or snake respectively. <br> First to reach tile 100 wins!
  * 
@@ -9,83 +9,89 @@ import java.util.Scanner;
  * @author Julian Lussier
  * @version 1.0 Feb 5, 2021
  */
-public class Playsnakeandladder {
+public class PlayLadderAndSnake {
 
 	public static void main(String[] args) throws InterruptedException {
+		
 		final int NB_PLAYERS_MIN = 2;
 		int  numChances = 0;
-		Scanner userInput = new Scanner(System.in);
-		printWelcomeBanner();
-		
-		System.out.print("\nPlease enter your name: ");
-		String userName = userInput.nextLine();
 		String playerLimit;
 		int maxPlayers;
 		String gameContinue = "y";
-		do{
-		do{
-			System.out.print( userName + ", Would you like to play extreme mode? (Y/N): \n");
-			 playerLimit = userInput.next();
-			if (playerLimit.equals("Y") || playerLimit.equals("y") )
-		maxPlayers = 100;
-	else 
-		maxPlayers = 4;}while(!(playerLimit.equals("Y") || playerLimit.equals("y") || playerLimit.equals("N") || playerLimit.equals("n")) );
+		String userName;
+		Scanner userInput = new Scanner(System.in);
+		
+		printWelcomeBanner();
+		System.out.print("\nPlease enter your name: ");
+		userName = userInput.nextLine();
+		
+		do {
+
+			//Offer normal (2-4 players) or extreme mode (4+ players)
+			do{
+				System.out.print( userName + ", Would you like to play extreme mode? (Y/N): \n");
+				playerLimit = userInput.next();
+				if (playerLimit.equals("Y") || playerLimit.equals("y") )
+					maxPlayers = 100;
+				else 
+					maxPlayers = 4;
+			} while(!(playerLimit.equals("Y") || playerLimit.equals("y") || playerLimit.equals("N") || playerLimit.equals("n")) );
 		
 
-		// User Input Validation 
-		System.out.print("Please enter amount of players (2-"+ maxPlayers + " players): ");
-		int numPlayers = userInput.nextInt();
-		while (numPlayers < NB_PLAYERS_MIN || numPlayers > maxPlayers) { // to change back
-			numChances++;
-			if (numChances == 4) {
-				System.out.println("You've run out of attempts");
-				printEndScreen();
-				System.exit(0);
+			// User Input Validation - Allows 4 attempts then terminates
+			System.out.print("Please enter amount of players (2-"+ maxPlayers + " players): ");
+			int numPlayers = userInput.nextInt();
+			while (numPlayers < NB_PLAYERS_MIN || numPlayers > maxPlayers) { // to change back
+				numChances++;
+				
+				if (numChances == 4) {
+					System.out.println("You've run out of attempts");
+					printEndScreen();
+					System.exit(0);
+				} else {
+				System.out.println("Attempt (" + numChances + "/4). Invalid number of players.");
+				numPlayers = userInput.nextInt();
+				}
 			}
-			else {
-			System.out.println("Attempt (" + numChances + "/4). Invalid number of players.");
-			numPlayers = userInput.nextInt();
+		
+			// Create Array of players with randomly generates player names
+			Players[] playersArray = new Players[numPlayers];
+			
+			playersArray[0] = new Players(userName);
+
+			for (int i=1; i < numPlayers; i++) {
+				int x =(int)(Math.random()*100);
+				playersArray[i] = new Players(Players.randNames[x]);
 			}
-		}
-		
-		// Create array of players
-		Players[] playersArray = new Players[numPlayers];
-		
-		playersArray[0] = new Players(userName);
 
-		for (int i=1; i < numPlayers; i++) {
-			int x =(int)(Math.random()*100);
-			playersArray[i] = new Players(Players.randNames[x]);
-		}
+			System.out.println("\nYou've chosen " + numPlayers + " players!\n");
+			loadingIcon("Generating friends ...");
+			
+			//Players flip dice until player order is determined
+			loadingIcon("Ordering players...");
+			Players.orderPlayers(playersArray, 0, playersArray.length-1);
+			
+			System.out.print("The final order of players is: \n- ");
+			for (int i=0; i < playersArray.length ; i++) {
+				System.out.print(playersArray[i].getPlayerName() + " - ");
+			}
 
-		//Players.flipNsort(playersArray, 0, playersArray.length-1);
-		//Players.printPlayerArray(playersArray);
+			// Create and start game
+			System.out.println("\n\n----  Let's start the game!  ----\n");
+			
+			LadderAndSnake game = new LadderAndSnake(playersArray);
 
-	System.out.println("\nYou've chosen " + numPlayers + " players!\n");
-		loadingIcon("Generating friends ...");
-		
-		loadingIcon("Ordering players...");
-		
-		Players.orderPlayers(playersArray, 0, playersArray.length-1);
-		
-		
-		System.out.print("The final order of players is: \n- ");
-        for (int i=0; i < playersArray.length ; i++) {
-			System.out.print(playersArray[i].getPlayerName() + " - ");
-		}
-		System.out.println("\n\n----  Let's start the game!  ----\n");
+			game.play();
 
+			//Offer user to play again
+			do { 
+				System.out.print("\nDo you want to Play again? (y/n): ");
+				gameContinue = userInput.next();
+			} while (!(gameContinue.equals("y") || gameContinue.equals("Y") || gameContinue.equals("n") || gameContinue.equals("N")));
 		
-		
-		LadderAndSnake game = new LadderAndSnake(playersArray);
+		} while (gameContinue.equals("Y") || gameContinue.equals("y"));
 
-		game.play();
-		do{System.out.print("\nDo you want to Play again? (y/n): ");
-		gameContinue = userInput.next();
-		}while(!(gameContinue.equals("y") || gameContinue.equals("Y") || gameContinue.equals("n") || gameContinue.equals("N")));
-		
-	}while(gameContinue.equals("Y") || gameContinue.equals("y"));
-
+		// Closing message
 		System.out.println("\nThe program has termintated\n");
 		userInput.close();
 	}
@@ -125,14 +131,14 @@ public class Playsnakeandladder {
 	 * prints game over is a fashionable display to console
 	 */
 	public static void printEndScreen() {
-				System.out.println("\n|<-------------------------------------------------------------------------------->|");
-				System.out.println("|<-------------------------------->| GAME OVER |<--------------------------------->|");
-				System.out.println("|<-------------------------------------------------------------------------------->|\n");
-			}
+		System.out.println("\n|<-------------------------------------------------------------------------------->|");
+		System.out.println("|<-------------------------------->| GAME OVER |<--------------------------------->|");
+		System.out.println("|<-------------------------------------------------------------------------------->|\n");
+	}
 			
-			/**
-			 * Prints an elaborate welcome banner to the program
-			 */
+	/**
+	 * Prints an elaborate welcome banner to the program
+	 */
 	public static void printWelcomeBanner(){
 		System.out.println("\n||<-----------------------------------------------/-----------/---------------------->||");
 		System.out.println("||                           ____________________/__________ /      ('-__--__--.      ||");
